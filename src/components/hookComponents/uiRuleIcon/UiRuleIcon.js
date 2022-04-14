c901b30aa9be4473a5eb8e1210cc5b94ffa0d546
Laptop <lidia.cirrone@gmail.com>
2022-04-14 08:54:36 +0200

    minor: UiRuleIcon + default icons

---
+++ src/components/hookComponents/uiRuleIcon/UiRuleIcon.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './UiRuleIcon.css';
+
+
+const UiRuleIcon = (props) => {
+
+   return (
+         <picture>
+            <img src={props.icon} alt="Icon" style={{
+               width: props.size,
+               height: props.size
+            }} />
+         </picture>
+   )
+
+}
+
+UiRuleIcon.defaultProps = {
+   size: 20
+}
+
+UiRuleIcon.propTypes = {
+   src: PropTypes.string.isRequired,
+   size: PropTypes.number
+}
+
+export default UiRuleIcon;
+
+
+
+
+
+// {/* <source media="(min-width:650px)" srcSet={props.icon} />
+//                 <source media="(min-width:465px)" srcSet={props.icon} /> */}
+
+
